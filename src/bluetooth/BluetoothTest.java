eab84fa66136e50c75154e6151e62ddc9385e7d8
Joao Grego <jpgrego@gmail.com>
2015-03-25 00:07:42 +0000

    Changes

--- src/bluetooth/BluetoothTest.java
+++ src/bluetooth/BluetoothTest.java
@@ -1,24 +1,35 @@
 package bluetooth;
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.Vector;
+import javax.bluetooth.*;
 
-import javax.bluetooth.DiscoveryListener;
-import javax.bluetooth.LocalDevice;
-import javax.bluetooth.RemoteDevice;
-import javax.bluetooth.DeviceClass;
-import javax.bluetooth.ServiceRecord;
-import javax.bluetooth.DiscoveryAgent;
-
-// based on bluecove.org example
+/**
+ * This program uses the BlueCove library to try to establish a bluetooth connection. 
+ * Initially it searches for devices and registers the services that they provide. 
+ * Afterwards, it asks the user which device he wishes to connect to.
+ * Finally, it attempts to send a message with the newly established connection.
+ * @author jpgrego
+ *
+ */
 public class BluetoothTest {
 	
+	static final UUID OBEX_FILE_TRANSFER = new UUID(0x1106);
+	
+	// must be Vector because of possible concurrent access
 	public static final Vector<RemoteDevice> devicesDiscovered = new Vector<RemoteDevice>();
+	public static final Vector<String> servicesFound = new Vector<String>();		// getDiscoveryAgent().searchServices(...) allows association of services to a device
 	public static Object inquiryCompletedEvent = new Object();
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
+		int count = 1, indexChoice = 9999;
+		UUID serviceUUID = (args != null && args.length > 0) ? new UUID(args[0], false) : OBEX_FILE_TRANSFER;
+		Scanner scanner = new Scanner(System.in);
+		
 		devicesDiscovered.clear();		
+		servicesFound.clear();
 		
 		synchronized(inquiryCompletedEvent) {
 			boolean inquiryHasStarted = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
@@ -29,12 +40,24 @@ public class BluetoothTest {
 				System.out.printf("%d devices were found\n", devicesDiscovered.size());
 			}
 		}
+		
+		System.out.printf("List of devices: \n");
+		for(RemoteDevice dev : devicesDiscovered) System.out.printf("%d) %s (%s)\n", count++, dev.getBluetoothAddress(), dev.getFriendlyName(false));
+		
+		// crude input validation
+		while(indexChoice >= 9999) {
+			System.out.printf("Choose device: ");		
+			indexChoice = scanner.nextInt();
+			System.out.println();
+		}
+		
+		//devicesDiscovered.get(indexChoice).get;
 	}
 	
 	static DiscoveryListener listener = new DiscoveryListener() {
 		public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
 			System.out.printf("%s found", btDevice.getBluetoothAddress());
-			devicesDiscovered.addElement(btDevice);
+			devicesDiscovered.add(btDevice);
 			
 			try {
 				System.out.printf(" with name %s\n", btDevice.getFriendlyName(false));
@@ -50,10 +73,22 @@ public class BluetoothTest {
 			}
 		}
 
-		public void serviceSearchCompleted(int arg0, int arg1) {			
+		public void serviceSearchCompleted(int transID, int respCode) {			
+			System.out.printf("Service search is complete\n");
 		}
 
-		public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {				
+		public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {	
+			String url;
+			for(int i = 0; i < servRecord.length; ++i) {
+				url = servRecord[i].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false); 
+				if(url != null) {
+					servicesFound.add(url);
+					DataElement serviceName = servRecord[i].getAttributeValue(0x0100);
+					
+					if(serviceName != null) System.out.printf("Service '%s' found", url);
+					else					System.out.printf("Service found", url);
+				}
+			}
 		}
 	};
 	
