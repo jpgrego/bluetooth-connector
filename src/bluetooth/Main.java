3116bb189c6ebb10c590562cf76b8156fcd48313
Joao Grego <jpgrego@gmail.com>
2015-03-27 21:04:05 +0000

    x

--- src/bluetooth/Main.java
+++ src/bluetooth/Main.java
@@ -2,18 +2,19 @@ package bluetooth;
 
 import java.io.IOException;
 import java.util.Scanner;
-import javax.bluetooth.BluetoothStateException;
+import java.util.List;
 import javax.bluetooth.RemoteDevice;
 
 public class Main {
-	public static void main(String[] args) throws BluetoothStateException,InterruptedException {
+	public static void main(String[] args) throws IOException,InterruptedException {
 		
 		BluetoothConnector btConnector = new BluetoothConnector();
 		int nDevices = 0, nServices = 0, count = 1, indexChoice = -1;
 		Scanner scanner;
 		RemoteDevice[] btDevices;
+		List<String> deviceServices;
 		
-		System.out.printf("Searching for devices...\n");
+		System.out.print("Searching for devices...\n");
 		
 		while(nDevices <= 0) {
 			nDevices = btConnector.searchDevices();
@@ -34,10 +35,9 @@ public class Main {
 		scanner = new Scanner(System.in);
 		// crude input validation
 		while(indexChoice <= -1 || indexChoice >= nDevices) {
-			System.out.printf("Choose device: ");		
+			System.out.print("Choose device: ");		
 			indexChoice = scanner.nextInt() - 1;
 		}
-		scanner.close();
 		
 		try {
 			System.out.printf("Searching what services are available for device %s...\n", btDevices[indexChoice].getFriendlyName(false));
@@ -48,5 +48,18 @@ public class Main {
 	
 		nServices = btConnector.searchServicesFromDevice(btDevices[indexChoice]);
 		System.out.printf("%d services found\n", nServices);
+		deviceServices = btConnector.getServicesFromDevice(btDevices[indexChoice]);
+		for(String service : deviceServices) System.out.printf("%d) %s", count++, service);
+		
+		indexChoice = -1;
+		count = 1;
+		while(indexChoice <= -1 || indexChoice >= deviceServices.size()) {
+			System.out.print("Choose service: ");
+			indexChoice = scanner.nextInt() - 1;
+		}
+		scanner.close();
+		
+		System.out.print("Sending test message through OBEX...\n");
+		btConnector.sendTestMessage(deviceServices.get(indexChoice));
 	}
 }
